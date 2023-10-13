dfe7b159dd0f43a3231e766421e0ce6e92e684ef
KshitijPatel7 <KshitijPatel7@users.noreply.github.com>
2023-10-13 06:11:24 +0530

    completed search result

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,17 +3,17 @@ import axios from "axios";
 const BASE_URL = "https://youtube138.p.rapidapi.com"
 
 const options = {
-    params: {
-      hl: 'en',
-      gl: 'US'
-    },
-    headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_YOUTUBE_API_KEY,
-      'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-    }
-  };
-
-  export const fetchDataFromApi = async (url) => {
-    const {data} = await axios.get(`${BASE_URL}/${url}`, options);
-    return data;
+  params: {
+    hl: 'en',
+    gl: 'US'
+  },
+  headers: {
+    'X-RapidAPI-Key': "85ded09400msh86ca0c11e8fa550p1c9f75jsn539e4c2ef751",
+    'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
   }
+};
+
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+}
